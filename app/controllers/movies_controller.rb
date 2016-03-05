f701cda7ec2bc0d02c69be1ba0f9d4e2200f7dae
jsitzman <jsitzman369@neo.tamu.edu>
2016-03-04 18:51:07 -0600

    final commit
    apparently my code wasn't committing to GitHub using the git command.
    This is the completed code version for rottenpotatoes.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,41 @@ class MoviesController < ApplicationController
   end
 
   def index
+    #part 1: make the Title and Release Date clickable and sortable
     @movies = Movie.all
-  end
+    @movies = Movie.order(params[:sort])
+    @column = params[:sort]
 
+    #part 2: make the checkboxes appear with corresponding ratings
+    if params[:ratings]
+      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort])
+    end
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings]
+    if !@selected_ratings
+      @selected_ratings = Hash.new
+    end
+
+    #part 3: remember the settings
+    @redirect = 0
+    if params[:sort]
+      session[:sort] = params[:sort]
+    elsif session[:sort]
+      @redirect = 1
+    end
+    
+    if params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @redirect = 1
+    end
+    
+    if @redirect == 1
+      flash.keep
+      redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
+    end
+  end
+  
   def new
     # default: render 'new' template
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    rating_array = Array.new
+    self.select("rating").uniq.each {|item| rating_array.push(item.rating)}
+    rating_array.sort.uniq
+  end
 end
